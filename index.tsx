7dbbb86c4bb51c39cb55d6f4f070f8910479451e
mbcx07 <87905389+mbcx07@users.noreply.github.com>
2026-08-27 12:38:14 -0700

    ui: mount R9 dashboard

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppR8';
+import App from './AppR9';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Could not find root element to mount to');
