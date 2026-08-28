c63b74af4936ec17af4e0a47eb1e8a1b23f3ec30
mbcx07 <87905389+mbcx07@users.noreply.github.com>
2026-08-27 17:40:50 -0700

    ui: expose R11 performance chart components

---
+++ components/PerformanceChartR11.tsx
@@ -0,0 +1,7 @@
+export {
+  PerformanceChartR10 as PerformanceChartR11,
+  AutoScanBadgeR10 as AutoScanBadgeR11,
+  buildCryptoCurveR10 as buildCryptoCurveR11,
+  buildForexCurveR10 as buildForexCurveR11,
+} from './PerformanceChartR10';
+export type { PerformancePoint } from './PerformanceChartR10';
